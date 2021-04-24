c6335f6cfb5c91933100b348e74eeca67542e35e
diamondt5353 <57979092+diamondt5353@users.noreply.github.com>
2021-04-24 19:11:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,7 +8,8 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+#dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
